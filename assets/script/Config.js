7ab4c0ef59801d1c2c50406130fae7a927e56c02
Ives She <ivesshe@gmail.com>
2020-04-26 17:26:26 +0800

    微調

--- assets/script/Config.js
+++
@@ -1,5 +0,0 @@
-//定義得分倍數
-const Config = Config || {};
-Config.gearInfo = ['2x', '6x', '2x', '4x', '5x', '4x', '8x', '4x', '3x', '5x', '3x', '10x', '6x', '2x', '2x', '3x', '10x', '8x'];
-
-module.exports = Config;
